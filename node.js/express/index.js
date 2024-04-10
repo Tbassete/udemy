f0ad95483d79eeb647634b7a9d22a28fabfff9f6
Thiago Bassete <taraujo438.ta@gmail.com>
2024-04-10 15:04:56 -0300

    s

--- node.js/express/index.js
+++ node.js/express/index.js
@@ -1,11 +1,21 @@
 import express from "express";
 const app = express();
-const port = 3000;
+const port = 5550;
+import bodyParser from "body-parser";
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+const __dirname = dirname(fileURLToPath(import.meta.url));
 
-app.get("/", (req,res) =>{
-    res.send("hello world jessuus");
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.get("/", (req, res) =>{
+    res.sendFile(__dirname +"/index.html");
 })
 
+app.post("/submit", (req, res) =>{
+    console.log(req.body)
+})
+/*
 app.get("/about", (req, res) =>{
     res.send("about")
 })
@@ -33,13 +43,15 @@ app.delete("/user/angela", (req, res)=> {
     res.sendStatus(200)
 })
 
-app.listen(port, ()=>{
-    console.log(`the server as running on port ${port}`)
-})
+
 
 //use this code to show you how or wich port is empty
 //netstat -ano | findstr "LISTENING"
 
 
 //use this on case powershell block the nodemon
-//Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Scope CurrentUser
+//Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Scope CurrentUser*/
+
+app.listen(port, ()=>{
+    console.log(`the server as running on port ${port}`)
+})
